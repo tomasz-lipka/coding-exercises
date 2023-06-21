40e8223387912bd29c479d6d2f753f333fc11558
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-06-21 14:44:56 +0000

    Fix comment

--- src/brainteasers/factorial/Factorial.java
+++ src/brainteasers/factorial/Factorial.java
@@ -6,8 +6,9 @@ import java.util.Scanner;
  * Exercise:
  * Implement a method which calculates the factorial of a given number.
  * Example:
+ * 0! = 1
  * 1! = 1
- * 2! = 1*2 =2
+ * 2! = 1*2 = 2
  * 3! = 1*2*3 = 6
  */
 public class Factorial {
