c19d385d13293759476b07722b96860de399bf85
tomasz-lipka <36009966+tomasz-lipka@users.noreply.github.com>
2023-10-31 10:38:47 +0000

    edit dosctring

--- src/brainteasers/stringrotation/stringrotation.py
+++ src/brainteasers/stringrotation/stringrotation.py
@@ -2,8 +2,9 @@
 From Cracking the Coding Interview, Exercise 1.9
 
 Assume you have a method isSubstring which checks if one word is a substring
-of another. Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one
-call to isSubstring (e.g., waterbottleu is a rotation of uerbottlewat ).
+of another. Given two strings, s1 and s2, write code to check if s2 is a 
+rotation of s1 using only one call to isSubstring (e.g., waterbottleu 
+is a rotation of uerbottlewat ).
 """
 
 import unittest
